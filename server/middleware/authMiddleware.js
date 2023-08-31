d4421abbb6b2df18165e3aa99cd05bc8deeff3d1
Seerat Shaikh <seeratshaikh579@gmail.com>
2023-08-31 10:46:29 +0500

    Login/Signup

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check json web token exists & is verified
+  if (token) {
+    jwt.verify(token, "Attendance", (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect("/login");
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+//app.get('/api', (req, res) => {
+   //res.json({ "users": ["user1", "user2", "user3"] })
+//})
+
+//const dbIRI = 'mongodb+srv://alibaba1990:19901990@cluster0.mcpgbvc.mongodb.net/?retryWrites=true&w=majority';
+const dbIRI = 'mongodb+srv://data70:cute12@cluster0.vk5gxdo.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbIRI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(5000))
+    .catch((err) => console.log(err));
+
+// app.listen(5000, () => console.log("listening"))
+
+
+app.get('/', (req, res) => res.render('home'));
+app.use(authRoutes);
